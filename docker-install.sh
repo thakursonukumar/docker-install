17c300083331475eebbafc2ba1fd6a58277c1f4a
Sonukumar Thakur <sonukumar.thakur@ukheshe.com>
2024-06-13 11:55:01 +0530

    shell script to install docker and docker-compose

---
+++ docker-install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Set up the stable repository for Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update the package list again
+sudo apt-get update
+# Install Docker Engine
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# Install Docker Compose
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+# Verify installation
+docker --version
+docker-compose --version
+echo "Docker and Docker Compose have been installed successfully."
